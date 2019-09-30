9d3f7beb35f5c914fa05304852fab702838419b1
Philip Schulz <Philip.Schulz.ps@gmail.com>
2019-09-30 21:01:58 +0200

    added texture-class

---
+++ brickstone/src/texture/texture.cpp
@@ -0,0 +1,40 @@
+#include "Texture.h"
+
+bs::Texture::Texture(const std::string & path) : path(path) {
+
+	stbi_set_flip_vertically_on_load(1);
+	this->buffer = stbi_load(this->path.c_str(), &(this->width), &(this->height), &(bpp), 4);
+
+	glGenTextures(1, &(this->id));
+	glBindTexture(GL_TEXTURE_2D, this->id);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, this->width, this->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (const void*)buffer);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	if (this->buffer)
+		stbi_image_free(this->buffer);
+}
+
+bs::Texture::~Texture() {
+
+	glDeleteTextures(1, &(this->id));
+
+}
+
+void bs::Texture::bind(const int& slot) {
+
+	glActiveTexture(GL_TEXTURE0 + slot);
+	glBindTexture(GL_TEXTURE_2D, this->id);
+
+}
+
+void bs::Texture::unbind() {
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+}
---
+++ brickstone/src/texture/texture.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <GL/glew.h>
+#include "../stb_image.h"
+
+namespace bs {
+
+	class Texture {
+	private:
+		
+		unsigned int id;
+		std::string path;
+		unsigned char* buffer;
+		int width, height;
+		int bpp;
+
+	public:
+		Texture(const std::string& path = "");
+		~Texture();
+
+		void bind(const int& slot = 0);
+		void unbind();
+
+	};
+
+}
