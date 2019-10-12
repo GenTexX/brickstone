3644aeaa51b699efc7f67dadcdbbf96c3d74d584
Andi1603 <andreas.schlager28@gmail.com>
2019-10-12 15:15:21 +0200

    Use EXPEXT_EQ instead of ASSERT_EQ.

--- test/BrickStone.Test/vector_tests.cpp
+++ test/BrickStone.Test/vector_tests.cpp
@@ -4,7 +4,7 @@ using namespace bs;
 
 #pragma region Fixture class
 
-class ivec2Test : public ::testing::Test {
+class ivec2_test : public ::testing::Test {
 protected:
 	void SetUp() override {
 		a_ = ivec2(0, 1);
@@ -33,46 +33,74 @@ protected:
 
 #pragma endregion
 
-TEST_F(ivec2Test, Add) {
+TEST_F(ivec2_test, Add) {
 	ivec2 ab = a_.add(b_);
 	ivec2 cd = c_.add(d_);
 	ivec2 ef = e_.add(f_);
 	ivec2 gh = g_.add(h_);
 	ivec2 ij = i_.add(j_);
 
-	ASSERT_EQ(1, ab.x);
-	ASSERT_EQ(1, ab.y);
-	ASSERT_EQ(0, cd.x);
-	ASSERT_EQ(0, cd.y);	
-	ASSERT_EQ(0, ef.x);
-	ASSERT_EQ(0, ef.y);	
-	ASSERT_EQ(0, gh.x);
-	ASSERT_EQ(0, gh.y);
-	ASSERT_EQ(0, ij.x);
-	ASSERT_EQ(0, ij.y);
+	EXPECT_EQ(1, ab.x);
+	EXPECT_EQ(1, ab.y);
+	EXPECT_EQ(0, cd.x);
+	EXPECT_EQ(0, cd.y);	
+	EXPECT_EQ(0, ef.x);
+	EXPECT_EQ(0, ef.y);	
+	EXPECT_EQ(0, gh.x);
+	EXPECT_EQ(0, gh.y);
+	EXPECT_EQ(0, ij.x);
+	EXPECT_EQ(0, ij.y);
 }
 
-TEST_F(ivec2Test, Sub) {
+TEST_F(ivec2_test, Sub) {
 	ivec2 ab = a_.sub(b_);
 	ivec2 cd = c_.sub(d_);
 	ivec2 ef = e_.sub(f_);
 	ivec2 gh = g_.sub(h_);
 	ivec2 ij = i_.sub(j_);
 
-	ASSERT_EQ(-1, ab.x);
-	ASSERT_EQ(1, ab.y);
-	ASSERT_EQ(2, cd.x);
-	ASSERT_EQ(2, cd.y);
-	ASSERT_EQ(-2, ef.x);
-	ASSERT_EQ(-2, ef.y);
-	ASSERT_EQ(-2, gh.x);
-	ASSERT_EQ(2, gh.y);
-	ASSERT_EQ(2, ij.x);
-	ASSERT_EQ(-2, ij.y);
+	EXPECT_EQ(-1, ab.x);
+	EXPECT_EQ(1, ab.y);
+	EXPECT_EQ(2, cd.x);
+	EXPECT_EQ(2, cd.y);
+	EXPECT_EQ(-2, ef.x);
+	EXPECT_EQ(-2, ef.y);
+	EXPECT_EQ(-2, gh.x);
+	EXPECT_EQ(2, gh.y);
+	EXPECT_EQ(2, ij.x);
+	EXPECT_EQ(-2, ij.y);
 }
 
-TEST(Math, ivec2_static_distance) {
-	ivec2 a(3, 0);
-	ivec2 b(0, 4);
+TEST_F(ivec2_test, Magnitude) {
+	EXPECT_FLOAT_EQ(1.0, a_.magnitude());
+	EXPECT_FLOAT_EQ(1.0, b_.magnitude());
+	EXPECT_FLOAT_EQ(1.4142135623f, c_.magnitude());
+	EXPECT_FLOAT_EQ(1.4142135623f, d_.magnitude());
+	EXPECT_FLOAT_EQ(1.4142135623f, e_.magnitude());
+	EXPECT_FLOAT_EQ(1.4142135623f, f_.magnitude());
+	EXPECT_FLOAT_EQ(1.4142135623f, g_.magnitude());
+	EXPECT_FLOAT_EQ(1.4142135623f, h_.magnitude());
+	EXPECT_FLOAT_EQ(1.4142135623f, i_.magnitude());
+	EXPECT_FLOAT_EQ(1.4142135623f, j_.magnitude());
+}
 
+TEST_F(ivec2_test, SqrMagnitude) {
+	EXPECT_EQ(1, a_.sqrMagnitude());
+	EXPECT_EQ(1, b_.sqrMagnitude());
+	EXPECT_EQ(2, c_.sqrMagnitude());
+	EXPECT_EQ(2, d_.sqrMagnitude());
+	EXPECT_EQ(2, e_.sqrMagnitude());
+	EXPECT_EQ(2, f_.sqrMagnitude());
+	EXPECT_EQ(2, g_.sqrMagnitude());
+	EXPECT_EQ(2, h_.sqrMagnitude());
+	EXPECT_EQ(2, i_.sqrMagnitude());
+	EXPECT_EQ(2, j_.sqrMagnitude());
+}
+
+TEST_F(ivec2_test, Equals) {
+	ASSERT_FALSE(a_.equals(b_));
+	ASSERT_FALSE(c_.equals(d_));
+	ASSERT_FALSE(e_.equals(f_));
+	ASSERT_FALSE(g_.equals(h_));
+	ASSERT_FALSE(i_.equals(j_));
 }
